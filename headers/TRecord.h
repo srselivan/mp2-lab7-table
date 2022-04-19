0fc30a733d290b952558e93971d8e27e4edef0d3
Sergey Selivankin <sergeyselivankin@gmail.com>
2022-04-19 22:32:51 +0300

    Init commit. Create abstract class TTable & struct TRecord

---
+++ headers/TRecord.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+typedef int TKey;
+typedef std::string TValue;
+
+struct TRecord {
+	TKey key;
+	TValue value;
+
+	bool operator > (const TRecord& rec) const {
+		return key > rec.key;
+	}
+
+	bool operator < (const TRecord& rec) const {
+		return key < rec.key;
+	}
+
+	friend std::ostream& operator<< (std::ostream& os, const TRecord rec) {
+		os << rec.key << " " << rec.value;
+		return os;
+	}
+};
---
+++ headers/Table.h
@@ -0,0 +1,44 @@
+#pragma once
+#include "TRecord.h"
+
+class TTable {
+protected:
+	int DataCount; 
+	int Eff;
+
+public:
+	TTable() : DataCount(0), Eff(0) {}
+
+	virtual ~TTable() {} 
+
+	virtual bool IsFull() const = 0;
+
+	virtual bool Find(TKey key) const = 0;
+
+	virtual bool Insert(TRecord rec) const = 0;
+
+	virtual bool Delete(TKey key) const = 0;
+
+	virtual void Reset() = 0;
+
+	virtual bool IsEnd() = 0;
+
+	virtual void GoNext() = 0;
+
+	virtual TRecord GetCurrRec() const = 0;
+
+	int GetDataCount() const { return DataCount; }
+
+	int GetEff() const { return Eff; }
+
+	void ClearEff() { Eff = 0; }
+
+	bool IsEmpty() const { return DataCount == 0; }
+
+	friend std::ostream& operator<< (std::ostream& os, TTable& t) {
+		for (t.Reset(); !t.IsEnd(); t.GoNext()) {
+			os << t.GetCurrRec() << std::endl;
+		}
+		return os;
+	}
+};
